2068ed8fba4545b2c18e20ae83e09651e0bcd85b
Eveline Guerrero <evelineguerrero23@gmail.com>
2021-12-20 10:14:31 -0500

    commit tree 3937fe9da8dfaf6af33425cc798c6e248039523b

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+//create a function that usese the find() method 
+//takes in an array of objects 
 
+//function is called superBowlWin()
+
+function superbowlWin(record){
+    const result = record.find(obj => obj.result === "W");
+    if (result) {
+        return result.year;
+    } else {
+        return undefined;
+    }
+}
+
+
+
+
+    
